235db24e4232c4c8aea7728db39155471ba05ae6
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-27 16:29:04 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
