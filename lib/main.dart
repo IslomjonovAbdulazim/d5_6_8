e0be1488e7fa0fc8d5b6180f78cbae0eb20c5d1b
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-27 16:58:48 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:liquid_progress_indicator_v2/liquid_progress_indicator.dart';
 
 void main() {
   runApp(
     DevicePreview(
+      enabled: false,
       builder: (context) => MyApp(),
     ),
   );
@@ -29,6 +31,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int cups = 0;
+  double prevValue = 0.0;
+  double currValue = 0.0;
+  int currCups = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +43,32 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(),
+          child: Column(
+            children: [
+              // Tank
+              Center(
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: TweenAnimationBuilder(
+                    tween: Tween<double>(begin: prevValue, end: currValue),
+                    duration: Duration(milliseconds: 700),
+                    builder: (context, value, child) {
+                      return LiquidCircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(Color(0xff5DCCFC)),
+                        backgroundColor: Color(0xffF2F2F2),
+                        borderColor: Color(0xff5DCCFC),
+                        borderWidth: 3,
+                        value: value,
+                      );
+                    },
+                  ),
+                ),
+              ),
+
+              // Buttons
+            ],
+          ),
         ),
       ),
     );
